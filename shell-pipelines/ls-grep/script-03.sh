4cbb0aa16e5cc6e4e66ae6da416dda038d28c5d4
Sara Tahir <saratahir34@gmail.com>
2025-11-13 11:44:18 +0000

    Answering all exercises for ls-grep folder

--- shell-pipelines/ls-grep/script-03.sh
+++ shell-pipelines/ls-grep/script-03.sh
@@ -4,3 +4,6 @@ set -euo pipefail
 
 # TODO: Write a command to output the names of the files in the sample-files directory whose name starts with an upper case letter and doesn't contain any other upper case letters.
 # Your output should contain 7 files.
+ls sample-files/| grep '^[A-Z][^A-Z]*$'
+
+# [^A-Z]*S A character that is not an uppercase letter ^ inside brackets means negation, * shows zero occurrences of it and $ ensures we are matching the whole file name
