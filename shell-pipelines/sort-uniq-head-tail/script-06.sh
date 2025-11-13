fb841b6286253ffbc11dfc2c2511bffa4c4a8e46
Sara Tahir <saratahir34@gmail.com>
2025-11-13 13:01:42 +0000

    Answering all questions for head,tail,sort,uniq folder

--- shell-pipelines/sort-uniq-head-tail/script-06.sh
+++ shell-pipelines/sort-uniq-head-tail/script-06.sh
@@ -1,7 +1,8 @@
-#!/bin/bash
+awk#!/bin/bash
 
 set -euo pipefail
 
 # The input for this script is the events.txt file.
 # TODO: Write a command to show how many times anyone has entered and exited.
 # It should be clear from your script's output that there have been 5 Entry events and 4 Exit events.
+awk '{print $1}' events.txt | sort | uniq -c
