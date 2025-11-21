394036e0633ebff85196ef619f3954121bf70768
Sara Tahir <saratahir34@gmail.com>
2025-11-21 15:56:14 +0000

    Adding implementation for -a flag

--- implement-shell-tools/ls/ls.js
+++ implement-shell-tools/ls/ls.js
@@ -6,18 +6,27 @@ program
   .name("ls")
   .description("list directory contents")
   .option("-1, --one", "Outputs are printed one entry per line")
+  .option("-a, --all","Show all files including hidden files that start with a .")
   .argument("[directory]", "Directory to list", "."); // "." means current directory
 
 //interpret the program
 program.parse();
+const options = program.opts();
 const directory = program.args[0] || "."; //get dir arg- 1st arg in program.args array || if no arg default to current dir
 
 
-const files = await fs.readdir(directory); //read the dir to get array of filenames
+let files = await fs.readdir(directory); //read the dir to get array of filenames
 
 
+if (!options.all) { //Handle -a (include hidden files)
+  files = files.filter(name => !name.startsWith("."));
+}
 
-//print each file on its own line
-for (const file of files) { // Loop prints each individually on separate lines
-    console.log(file)
+if (options.one) { // Print each file on its own line
+  for (const file of files) {
+    console.log(file);
+  }
+}
+else {
+  console.log(files.join(" "));// Default: join with spaces (like ls without -1)
 }
